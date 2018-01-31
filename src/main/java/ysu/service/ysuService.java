96e7ecc2de4d609e956c93a00da1cf379e78b559
chujinshuai <chujinshuai@meicai.cn>
2018-01-31 11:32:55 +0800

    燕山大学图书共享

---
+++ src/main/java/ysu/service/ysuService.java
@@ -0,0 +1,14 @@
+package ysu.service;
+
+/**
+ * Title: <br>
+ * Description: <br>
+ * Copyright: Copyright (c) 2017<br>
+ * Company: 北京云杉世界信息技术有限公司<br>
+ *
+ * @author :chujinshuai
+ * @time :2018/1/31 10:53 星期三
+ */
+public class ysuService
+{
+}
